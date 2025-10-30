79797c5b0ea6d8ffb9b3e29aeb15f30b41824e3a
maheshsangam225 <132155457+maheshsangam225@users.noreply.github.com>
2025-10-30 16:13:30 +0530

    Add files via upload

---
+++ LoginDTO.java
@@ -0,0 +1,31 @@
+package com.example.demo.DTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class LoginDTO {
+    @Email
+    @NotBlank
+    private String Email;
+    @NotBlank
+    private String Password;
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+}
---
+++ RegisterDTO.java
@@ -0,0 +1,109 @@
+package com.example.demo.DTO;
+
+import java.time.LocalDate;
+
+import com.example.demo.Enum.Role;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class RegisterDTO {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Role role;
+    private String gender;
+    private LocalDate dateOfBirth;
+    private String address;
+    private String phoneNumber;
+    private String emergencyContact;
+    private String specialisation;
+    private String licenseNo;
+    private String hospitalName;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+	this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmergencyContact() {
+		return emergencyContact;
+	}
+	public void setEmergencyContact(String emergencyContact) {
+		this.emergencyContact = emergencyContact;
+	}
+	public String getSpecialisation() {
+		return specialisation;
+	}
+	public void setSpecialisation(String specialisation) {
+		this.specialisation = specialisation;
+	}
+	public String getLicenseNo() {
+		return licenseNo;
+	}
+	public void setLicenseNo(String licenseNo) {
+		this.licenseNo = licenseNo;
+	}
+	public String getHospitalName() {
+		return hospitalName;
+	}
+	public void setHospitalName(String hospitalName) {
+		this.hospitalName = hospitalName;
+	}
+
+
+}
